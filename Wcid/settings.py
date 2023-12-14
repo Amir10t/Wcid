d1de3bd4040e61fb8aee775b7998412d95553c17
Amir <amirhasantalebi1400@gmail.com>
2023-12-14 20:29:05 +0330

    Fifth Commit

--- Wcid/settings.py
+++ Wcid/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "question_App"
 ]
 
 MIDDLEWARE = [
@@ -116,7 +117,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- question_App/views.py
+++ question_App/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def question(request):
+    return render(request, "question/single_question.html", {})
