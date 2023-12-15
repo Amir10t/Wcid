d929e12fc2762d15bcb4febb215f3ef429eb05ea
Amir <amirhasantalebi1400@gmail.com>
2023-12-15 15:01:14 +0330

    Six Commit, Create Question Model

--- question_App/models.py
+++ question_App/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class QuestionModel(models.Model):
+    question = models.CharField(max_length=300, verbose_name="سوال")
+    option1 = models.CharField(max_length=20, verbose_name="گزینه 1")
+    option2 = models.CharField(max_length=20, verbose_name="گزینه 2")
+    option3 = models.CharField(max_length=20, verbose_name="گزینه 3")
+    option4 = models.CharField(max_length=20, verbose_name="گزینه 4")
+    is_active = models.BooleanField(default=False, verbose_name='فعال / غیرفعال')
+    is_delete = models.BooleanField(verbose_name='حذف شده / نشده')
